21d6be7e75d01552d71618d7b750042f4ba658e4
angeldarco <angel92-eme@hotmail.com>
2023-04-16 01:31:14 -0500

    routes updated

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -1,21 +1,31 @@
-import React from 'react';
 import { Routes, Route } from 'react-router-dom';
-import Gmail from '../pages/gmail/Gmail';
 import Home from '../pages/home/Home';
 import Login from '../pages/login/Login';
 import PublicChat from '../pages/publicChat/PublicChat';
-import Register from '../pages/register/Register';
 import User from '../pages/user/User';
 import Welcome from '../pages/welcome/Welcome';
+import LoginEmail from '../pages/login/loginEmail/LoginEmail';
+import LoginGmail from '../pages/login/loginGmail/LoginGmail';
+import LoginPhone from '../pages/login/loginPhone/LoginPhone';
+
+import RegisterEmail from '../pages/register/RegisterEmail';
+
 
 const Router = (): JSX.Element =>{
 	return(
 		<Routes>
 			<Route path="/" Component={ Welcome } />
 			<Route path="/login" Component={Login } />
-			<Route path="/register" Component={ Register } />
-			<Route path="/gmail" Component={ Gmail } />
 
+			{/* Routes to Login options */}
+			<Route path="/email" Component={ LoginEmail } />
+			<Route path="/gmail" Component={ LoginGmail } />
+			<Route path="/phone" Component={ LoginPhone } />
+
+			{/* Routes to Register options  */}
+			<Route path="/register" Component={ RegisterEmail } />
+
+			{/* Routes to Public access */}
 			<Route path="/user" Component={ User } />
 			<Route path="/home" Component={ Home } />
 			<Route path="/public" Component={ PublicChat } />
