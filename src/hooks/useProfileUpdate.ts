5abbe86abada69767341418813d67ed7b5ba55ab
angeldarco <angel92-eme@hotmail.com>
2023-04-18 20:41:30 -0500

    :sparkles: configuration to update users profiles

---
+++ src/hooks/useProfileUpdate.ts
@@ -0,0 +1,26 @@
+import { getDownloadURL, getStorage, ref, uploadBytes } from 'firebase/storage';
+import useRealTimeDB from './useRealTimeDB';
+
+const useProfileUpdate = () => {
+	const { writeUserData, readUserData } = useRealTimeDB();
+
+	const storageImgs = async (fileName: string, file: Blob | undefined):Promise<string | undefined> =>{
+		if(!file) return;
+		try {
+			const storage = getStorage();
+			const imagesRef = ref(storage, 'profiles/' + fileName);
+			await uploadBytes(imagesRef, file);
+			const res = await getDownloadURL(ref(imagesRef));
+			return res;
+		} catch(error) {
+			return (error as string);
+		}
+	};
+
+
+
+
+	return { storageImgs, writeUserData, readUserData };
+};
+export default useProfileUpdate;
+
