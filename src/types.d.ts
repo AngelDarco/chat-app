511903911d5a84875938c4bbfb16e7dd8b0788f2
angeldarco <angel92-eme@hotmail.com>
2023-04-21 00:35:11 -0500

    :sparkles: types for update users profiles added

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,34 +1,45 @@
 /* interface of Context provider */
-export interface intContext{
-    userName: string | null,
-    userUid: string | null,
-    login?: intContext,
-    setLogin?: React.Dispatch<React.SetStateAction<intContext>>
+export interface intContext {
+	userName: string | null,
+	userUid: string | null,
+	login?: intContext,
+	setLogin?: React.Dispatch<React.SetStateAction<intContext>>
 }
 
 // interface of messages send
-interface message { userName: string, message: string, messageSendTime: number}
+interface message { userName: string, message: string, messageSendTime: number }
 
 // interface to write in the realtime DB
 interface intWrite {
-    userName: string | null,
+	userName: string | null,
 	messageId: string,
-    userDB: string | null,
-    message: string | undefined,
-    messageSendTime: number
+	userDB: string | null,
+	message: string | undefined,
+	messageSendTime: number
 }
 
 /* add and update new messages */
-interface intUpdateUserData{
+interface intUpdateUserData {
 	messageId: string,
-	userName:string | null,
+	userName: string | null,
 	message: string | null,
 	userDB?: string | null,
 	messageSendTime: number
-	}
+}
 
 /* credentials login user  */
 interface intLoginUserData {
 	email: string,
 	password: string
+}
+
+/* interface to update profiles of users  */
+interface intWriteProfiles { 
+	photo?: string,
+	userUid: string, 
+	userName: string, 
+	lastName: string,
+	state?: string,
+	about?: string
+	file?: Blob | undefined,
 }
