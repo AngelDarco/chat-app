ebbda3d9538c1633f3c2f627133d4d8619f68d54
angeldarco <angel92-eme@hotmail.com>
2023-04-21 00:40:37 -0500

    :sparkles: context and router added

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
+import './App.css';
+import { BrowserRouter } from 'react-router-dom';
+import Main from './pages/main/Main';
+import { MainContext } from './context/Context';
 
-const App = ()=>{
+const App: React.FC = () =>{
 	return(
-		<>
-			<h1> Working on it ...</h1>
-		</>
+		<MainContext>
+			<BrowserRouter>
+				<Main />
+			</BrowserRouter>
+		</MainContext>
 	);
 };
   
