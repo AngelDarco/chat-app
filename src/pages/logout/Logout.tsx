ee1252112ffcb3fe2e2e74f486a2143f69c3679f
angeldarco <angel92-eme@hotmail.com>
2023-05-24 10:41:24 -0500

    :bug: logout double prompt and renders bugs fixed

--- src/pages/logout/Logout.tsx
+++ src/pages/logout/Logout.tsx
@@ -4,12 +4,13 @@ import Loading from 'react-loading';
 import userContexUpdate from '../../utils/useContextUpdate';
 
 const Logout = () => {
+	const { deleteUserContext } = userContexUpdate();
 	const navigate = useNavigate();
-	const uid = window.localStorage.getItem('chatDarcoUserUid');	
 	
-	const handlerLogout = () => {
-		const { deleteUserContext } = userContexUpdate();
-		uid &&
+	const uid = window.localStorage.getItem('chatDarcoUserUid');
+	
+	const handlerLogout = () => {		
+		uid ?
 			Swal.fire({
 				title: 'You wanna close session?',
 				icon: 'warning',
@@ -27,15 +28,15 @@ const Logout = () => {
 									showConfirmButton: false,
 									timer: 1000
 								}
-								), setTimeout(() => navigate('/'), 1100))
+								), setTimeout(() => navigate('/'), 2000))
 								: (Swal.fire({
 									title: res,
 									icon: 'error',
 									timer: 1000
-								}), setTimeout(() => navigate(-1), 1100));
+								}), setTimeout(() => navigate(-1), 2000));
 						});
 				}else{ navigate(-1); }
-			});
+			})	: setTimeout(() => navigate('/'), 1000);
 		return <Loading
 			type='cylon'
 			color='green'
