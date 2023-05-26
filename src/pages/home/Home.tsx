2fc8413178a1ed31dc0002054a5c5edf60256c9f
angeldarco <angel92-eme@hotmail.com>
2023-05-26 10:58:56 -0500

    :hammer: protected route added for no logged users

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -3,9 +3,21 @@ import styles from './home.module.css';
 import { FiSearch } from 'react-icons/fi';
 import logo from '../../assets/img/ico.png';
 import { headerUser } from '../../components/header/Header';
+import { useEffect, useState } from 'react';
+import { intContext } from '../../types';
+import userContexUpdate from '../../utils/useContextUpdate';
+import Loading from 'react-loading';
+import MessageNoLogged from '../../components/messageNoLog/MessageNoLogged';
 
 const User = (): JSX.Element => {
-	const user: string | boolean = 'false';
+	const { userContextData } = userContexUpdate(); 
+	const [ userData, setUserData ] = useState<intContext>();
+	useEffect(()=>{
+		userContextData()
+			.then(res => setUserData(res))
+			.catch(err => console.log(err));
+	},[userData?.userUid]);
+
 	const contacts = [
 		{
 			img: logo,
@@ -23,47 +35,55 @@ const User = (): JSX.Element => {
 	return (
 		<div className={styles.containerUser}>
 			<Header props={headerUser} />
-			{!user ? <h5> you must be loged to see this section </h5> :
-				<div className={styles.userContent}>
-					<div className={styles.header}>
-						<div>
-							<img src={logo} alt="user-logo" />
-							<FiSearch />
+			{!userData ? 
+				<Loading
+					type='cylon'
+					color= 'green'
+					className={'loader'}
+				/>
+				: !userData.userUid ?
+					<MessageNoLogged/>
+					:
+					<div className={styles.userContent}>
+						<div className={styles.header}>
+							<div>
+								<img src={logo} alt="user-logo" />
+								<FiSearch />
+							</div>
+							<p>Messages</p>
 						</div>
-						<p>Messages</p>
-					</div>
-					<div className={styles.contacts}>
-						<ul>
-							{
-								contacts.map((item, i) => {
-									return (
-										<li key={i}>
-											<img src={item.img} />
-											<span>{item.name}</span>
-										</li>
-									);
-								})
-							}
-						</ul>
-					</div>
-					<div className={styles.chats}>
-						<ul>
-							{
-								contacts.map((item, i) => {
-									return (
-										<li key={i}>
-											<img src={item.img} />
-											<div className={styles.context}>
+						<div className={styles.contacts}>
+							<ul>
+								{
+									contacts.map((item, i) => {
+										return (
+											<li key={i}>
+												<img src={item.img} />
 												<span>{item.name}</span>
-												<p>text of last message</p>
-											</div>
-										</li>
-									);
-								})
-							}
-						</ul>
+											</li>
+										);
+									})
+								}
+							</ul>
+						</div>
+						<div className={styles.chats}>
+							<ul>
+								{
+									contacts.map((item, i) => {
+										return (
+											<li key={i}>
+												<img src={item.img} />
+												<div className={styles.context}>
+													<span>{item.name}</span>
+													<p>text of last message</p>
+												</div>
+											</li>
+										);
+									})
+								}
+							</ul>
+						</div>
 					</div>
-				</div>
 			}
 		</div>
 	);
