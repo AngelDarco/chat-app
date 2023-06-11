0409d88a0b947c5d725744579419f083c8fc37d6
Angel Darco <angel92-eme@hotmail.com>
2023-06-11 10:35:02 -0500

    :sparkles: profile card presentation module created

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable indent */
 import Header from '../../components/header/Header';
 import styles from './home.module.css';
 import { FiSearch } from 'react-icons/fi';
@@ -8,98 +9,80 @@ import { intContext } from '../../types';
 import userContexUpdate from '../../utils/useContextUpdate';
 import Loading from 'react-loading';
 import MessageNoLogged from '../../components/messageNoLog/MessageNoLogged';
+import useRealTimeDB from '../../hooks/useRealTimeDB';
+import ProfileCard from '../../components/profileCard/ProfileCard';
 
 const User = (): JSX.Element => {
-	const { userContextData } = userContexUpdate(); 
-	const [ userData, setUserData ] = useState<intContext>();
-	useEffect(()=>{
-		userContextData()
-			.then(res => setUserData(res))
-			.catch(err => console.log(err));
-	},[userData?.userUid]);
+  const { userContextData } = userContexUpdate();
+  const [ userData, setUserData ] = useState<intContext>();
+  const [ contacts, setContacts ] = useState<intContext[]>();
 
-	const contacts = [
-		{
-			img: logo,
-			name: 'Angel'
-		},
-		{
-			img: logo,
-			name: 'Marco'
-		},
-		{
-			img: logo,
-			name: 'Darco'
+  /** read and update userdata */
+  useEffect(() => {
+    userContextData()
+      .then((res) => setUserData(res))
+      .catch((err) => console.log(err));
+  }, [ userData?.userUid ]);
+
+  /** read all the users profiles */
+  useEffect(() => {
+    const { readUserData } = useRealTimeDB();
+    (async () => {
+      const res = await readUserData('/profiles/');
+      const arr: intContext[] = Object.values(res as intContext);
+      setContacts(arr);
+    })();
+  }, []);
+
+  return (
+    <div className={styles.containerUser}>
+      <Header props={headerUser} />
+      {!userData ? (
+        <Loading type="cylon" color="green" className={'loader'} />
+      ) : !userData.userUid ? (
+        <MessageNoLogged />
+      ) : (
+        <div className={styles.userContent}>
+          <div style={style}>Sorry, this function is not available yet, but we are working on it!</div>
+          <div className={styles.header}>
+            <div>
+              <img src={logo} alt="user-logo" />
+              <FiSearch />
+            </div>
+            <p>Messages</p>
+          </div>
+          {
+			contacts &&
+			<ProfileCard
+				style={styles.contacts}
+				contacts={contacts}
+				limit={3}
+			/>
 		}
-	];
-	return (
-		<div className={styles.containerUser}>
-			<Header props={headerUser} />
-			{!userData ? 
-				<Loading
-					type='cylon'
-					color= 'green'
-					className={'loader'}
-				/>
-				: !userData.userUid ?
-					<MessageNoLogged/>
-					:
-					<div className={styles.userContent}>
-						<div style={style}>Sorry, this function is not available yet, but we are working on it!</div>
-						<div className={styles.header}>
-							<div>
-								<img src={logo} alt="user-logo" />
-								<FiSearch />
-							</div>
-							<p>Messages</p>
-						</div>
-						<div className={styles.contacts}>
-							<ul>
-								{
-									contacts.map((item, i) => {
-										return (
-											<li key={i}>
-												<img src={item.img} />
-												<span>{item.name}</span>
-											</li>
-										);
-									})
-								}
-							</ul>
-						</div>
-						<div className={styles.chats}>
-							<ul>
-								{
-									contacts.map((item, i) => {
-										return (
-											<li key={i}>
-												<img src={item.img} />
-												<div className={styles.context}>
-													<span>{item.name}</span>
-													<p>text of last message</p>
-												</div>
-											</li>
-										);
-									})
-								}
-							</ul>
-						</div>
-					</div>
+         {
+		contacts &&
+		<ProfileCard 
+			style={styles.chats} 
+			contacts={contacts} 
+			showState={true} />
 			}
-		</div>
-	);
+		
+        </div>
+      )}
+    </div>
+  );
 };
 export default User;
 const style: React.CSSProperties = {
-	width: '100%',
-	height: '100%',
-	top: '0',
-	position: 'absolute',
-	backgroundColor: '#000000D0',
-	color : 'white',
-	display: 'flex',
-	alignItems: 'center',
-	justifyContent: 'center',
-	fontSize: '28px',
-	padding: '15px',
+  width: '100%',
+  height: '100%',
+  top: '0',
+  position: 'absolute',
+  backgroundColor: '#000000D0',
+  color: 'white',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+  fontSize: '28px',
+  padding: '15px',
 };
