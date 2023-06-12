3db5c50b3a49f88c7eccf134bb40af5f20950d7b
Angel Darco <angel92-eme@hotmail.com>
2023-06-12 09:34:22 -0500

    :construction: component chat form added

--- src/components/messages/Messages.tsx
+++ src/components/messages/Messages.tsx
@@ -2,37 +2,37 @@ import { ReactNode, useEffect, useRef } from 'react';
 import styles from './messages.module.css';
 import { message } from '../../types';
 
-interface Props { messages: message[] }
+interface Props {
+  messages: message[];
+}
+/** recieve the messages from database, add new messages and render them */
 const Messages = (messages: Props): JSX.Element => {
-	const containerRef = useRef<HTMLDivElement>(null);
+  const containerRef = useRef<HTMLDivElement>(null);
 
-	useEffect(() => {
-		const container = containerRef.current;
-		container?.lastElementChild?.scrollIntoView({
-			behavior: 'auto',
-			block: 'end'
-		});
-	}, [messages.messages.length]);
+  useEffect(() => {
+    const container = containerRef.current;
+    container?.lastElementChild?.scrollIntoView({
+      behavior: 'auto',
+      block: 'end',
+    });
+  }, [ messages.messages.length ]);
 
-	return (
-		<div ref={containerRef} className={styles.publicChatMessages}>
-			{
-				messages.messages &&
-				messages?.messages.map((item: message): ReactNode => {
-					const time = new Date(item.messageSendTime).toLocaleString();
-					return (
-						<div key={item.messageSendTime} className={styles.messageSend}>
-							<span>{item.userName}</span>
-							<div className={styles.message}>
-								<span>{item.message}</span>
-								<span className=
-									{styles.messageTime}>send on: {time}</span>
-							</div>
-						</div>
-					);
-				})
-			}
-		</div>
-	);
+  return (
+    <div ref={containerRef} className={styles.publicChatMessages}>
+      {messages.messages &&
+        messages?.messages.map((item: message): ReactNode => {
+          const time = new Date(item.messageSendTime).toLocaleString();
+          return (
+            <div key={item.messageSendTime} className={styles.messageSend}>
+              <span>{item.userName}</span>
+              <div className={styles.message}>
+                <span>{item.message}</span>
+                <span className={styles.messageTime}>send on: {time}</span>
+              </div>
+            </div>
+          );
+        })}
+    </div>
+  );
 };
 export default Messages;
--- src/pages/publicChat/PublicChat.tsx
+++ src/pages/publicChat/PublicChat.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from 'react';
+import { useEffect, useState } from 'react';
 import Header, { headerUser } from '../../components/header/Header';
 import styles from './publicChat.module.css';
 import { v4 as uuidv4 } from 'uuid';
@@ -8,106 +8,80 @@ import { intContext, intUpdateUserData, message } from '../../types';
 import Loading from 'react-loading';
 import LoginGuests from '../../components/loginguests/LoginGuests';
 import userContexUpdate from '../../utils/useContextUpdate';
+import SendMessagesForm from '../../components/sendMessagesForm/SendMessagesForm';
 
 const PublicChat = (): JSX.Element => {
-	// get firebase functions
-	const { readUserData, updateUserData } = useRealTimeDB();
-	
-	// get the user context data
-	const { userContextData } = userContexUpdate();
+  // get firebase functions
+  const { readUserData, updateUserData } = useRealTimeDB();
 
-	//store and update user context data
-	const [ userData, setUserData ] = useState<intContext>();
-	const [messages, setMessages] = useState<message[]>();
+  // get the user context data
+  const { userContextData } = userContexUpdate();
 
-	/* enable send messages options */
-	// useref of input of the messages to be sended
-	const messageRef = useRef<HTMLTextAreaElement>(null);
-	const btnSendRef = useRef<HTMLButtonElement>(null);
-	useEffect(() => {
-		// update the user context data
-		userContextData()
-			.then(res => setUserData(res))
-			.catch(err => console.log(err));
+  //store and update user context data
+  const [ userData, setUserData ] = useState<intContext>();
+  const [ messages, setMessages ] = useState<message[]>();
 
-		// get the public messages
-		if(userData?.userName)
-			getData();
+  /* enable send messages options */
+  useEffect(() => {
+    // update the user context data
+    userContextData()
+      .then((res) => setUserData(res))
+      .catch((err) => console.log(err));
 
-		/** avalibility of the send button */
-		let availability = true;
-		const input = messageRef.current;
-		const btn = btnSendRef.current;
-		if (btn && input) {
-			if (userData?.userName?.trim()) availability = false;
-			btn.disabled = availability; input.disabled = availability;
-		}
-	}, [userData?.userName]);
+    // get the public messages
+    if (userData?.userName) getData();
+  }, [ userData?.userName ]);
 
-	const updateLocalUserData = (data: intContext):void =>{
-		setUserData(data);
-	};
+  const updateLocalUserData = (data: intContext): void => {
+    setUserData(data);
+  };
 
-	// send the messages to the firebase server
-	const handlerMessage = (e: React.FormEvent<HTMLFormElement>) => {
-		e.preventDefault();
-		if (messageRef && messageRef.current) {
-			const text = messageRef.current.value;
-			if (!text || text.trim() === '') return;
+  /* get messages from firebase server */
+  async function getData() {
+    try {
+      const userData = await readUserData<message[]>('/public/');
+      setMessages(() => userData);
+    } catch (error) {
+      console.error(error);
+    }
+  }
 
-			const writeData: intUpdateUserData = {
-				userDB: undefined,
-				messageId: uuidv4(),
-				userName: userData?.userName || null,
-				message: text,
-				messageSendTime: new Date().getTime(),
-			};
-			updateUserData(writeData)
-				.then(() => getData());
-			(e.target as HTMLFormElement).reset();
-		}
-	};
+  // write messages to the firebase server
+  const handlerSendMessages = (text: string) => {
+    const writeData: intUpdateUserData = {
+      userDB: undefined,
+      messageId: uuidv4(),
+      userName: userData?.userName || null,
+      message: text,
+      messageSendTime: new Date().getTime(),
+    };
+    updateUserData(writeData)
+      .then(() => getData());    
+  };
 
-	/* get messages from firebase server */
-	async function getData() {
-		try {
-			const userData = await readUserData<message[]>('/public/');
-			setMessages(() => userData);
-		} catch (error) {
-			console.error(error);
-		}
-	}
 
-	return (
-		<div className={styles.containerPublicChat}>
-			<Header props={headerUser} />
-			<div className={styles.publicChatContainer}>
-				{
-					!userData ? <Loading
-						type='cylon'
-						color='green'
-						className='loader'
-					/>	:
-						!userData?.userName ?
-							<LoginGuests updateLocalUserData={updateLocalUserData} /> :
-							messages ?
-								<Messages messages={messages} />
-								: <Loading
-									type='cylon'
-									color='green'
-									className='loader'
-								/>
-				}
-				<div className={styles.publicChatFunctions}>
-					<form onSubmit={handlerMessage}>
-						<textarea ref={messageRef} placeholder='message' disabled />
-						<button ref={btnSendRef} disabled>Send</button>
-					</form>
-				</div>
-			</div>
-		</div>
-	);
+
+  return (
+    <div className={styles.containerPublicChat}>
+      <Header props={headerUser} />
+      <div className={styles.publicChatContainer}>
+        {
+          !userData ? 
+            <Loading type="cylon" color="green" className="loader" />
+            : !userData?.userName ?
+              <LoginGuests updateLocalUserData={updateLocalUserData} />
+              : messages ? 
+                <Messages messages={messages} />
+                :
+                <Loading type="cylon" color="green" className="loader" />
+        }
+        <SendMessagesForm
+          userData={userData}
+          handlerSendMessages={handlerSendMessages}
+        />
+      </div>
+    </div>
+  );
 };
-
 
 export default PublicChat;
