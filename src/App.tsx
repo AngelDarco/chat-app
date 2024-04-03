3ff406bd91feb745364feb29377777a1601b9aea
angeldarco <angel92-eme@hotmail.com>
2024-04-03 09:17:05 -0500

    :racehorse: Register components refactored

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,17 @@
-import React from 'react';
-import './App.css';
-import { BrowserRouter } from 'react-router-dom';
-import Main from './pages/main/Main';
-import { MainContext } from './context/Context';
+import React from "react";
+import "./App.css";
+import { BrowserRouter } from "react-router-dom";
+import Main from "./pages/main/Main";
+import { MainContext } from "./context/Context";
 
-const App: React.FC = () =>{
-	return(
-		<MainContext>
-			<BrowserRouter>
-				<Main />
-			</BrowserRouter>
-		</MainContext>
-	);
+const App: React.FC = () => {
+  return (
+    <MainContext>
+      <BrowserRouter>
+        <Main />
+      </BrowserRouter>
+    </MainContext>
+  );
 };
-  
+
 export default App;
--- src/hooks/useRegisterUsers.ts
+++ src/hooks/useRegisterUsers.ts
@@ -1,18 +1,25 @@
-import { getAuth, createUserWithEmailAndPassword  } from 'firebase/auth';
-import { intLoginUserData } from '../types';
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+import { intLoginUserData } from "../types";
 
 const useRegisterUsers = () => {
-	const auth = getAuth();
+  const auth = getAuth();
 
-	const createAcountWithEmail = async ({email,password}:intLoginUserData) => {
-		try {
-			const userCredential = await createUserWithEmailAndPassword(auth, email, password);
-			return userCredential.user;
-		} catch (err) {
-			return err;
-		}
-	};
+  const createAcountWithEmail = async ({
+    email,
+    password,
+  }: intLoginUserData) => {
+    try {
+      const userCredential = await createUserWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
+      return userCredential.user;
+    } catch (err) {
+      return err;
+    }
+  };
 
-	return { createAcountWithEmail };
+  return { createAcountWithEmail };
 };
 export default useRegisterUsers;
