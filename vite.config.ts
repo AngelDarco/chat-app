2504a61aabe383821de85c999f8f0b38b607f0de
angeldarco <angel92-eme@hotmail.com>
2024-04-23 14:26:21 -0500

    :wrench: vite config updated

--- vite.config.ts
+++ vite.config.ts
@@ -1,30 +1,28 @@
 /// <reference types="vitest" />
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import reactRefresh from '@vitejs/plugin-react-refresh';
-import autoprefixer from 'autoprefixer';
-import cssnano from 'cssnano';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import autoprefixer from "autoprefixer";
+import cssnano from "cssnano";
 
 export default defineConfig({
-	plugins: [
-		react(),
-		reactRefresh()
-	],
-	css: {
-		modules: {
-			localsConvention: 'camelCase',
-		},
-		postcss: {
-			plugins: [
-				autoprefixer,
-				cssnano({
-					preset: 'default',
-				})			
-			],
-		},
-	},
-	test:{	// config to vitest 
-		environment: 'jsdom',
-		globals: true
-	}
+  plugins: [react()],
+  css: {
+    modules: {
+      localsConvention: "camelCase",
+    },
+    postcss: {
+      plugins: [
+        autoprefixer,
+        cssnano({
+          preset: "default",
+        }),
+      ],
+    },
+  },
+  test: {
+    // config to vitest
+    environment: "jsdom",
+    globals: true,
+  },
+  assetsInclude: ["**/*.glb"],
 });
