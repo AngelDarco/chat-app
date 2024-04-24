7fe2e392b5a57cdad7aade789dc83e61049e7c2e
angeldarco <angel92-eme@hotmail.com>
2024-04-24 13:43:11 -0500

    :lipstick: PersonalChat component styles updated

--- src/pages/personalChat/PersonalChat.tsx
+++ src/pages/personalChat/PersonalChat.tsx
@@ -1,30 +1,32 @@
-import Header, { headerPersonalChats } from '../../components/header/Header';
-import SendMessagesForm from '../../components/sendMessagesForm/SendMessagesForm';
-import styles from './personalchat.module.css';
-import { intAddFriend, intAddPersonalMessage, message } from '../../types';
-import { useEffect, useState } from 'react';
-import { useLocation } from 'react-router';
-import useRealTimeDB from '../../hooks/useRealTimeDB';
-import { v4 as uuidv4 } from 'uuid';
-import Messages from '../../components/messages/Messages';
-import Loading from 'react-loading';
-
+import Header, { headerPersonalChats } from "../../components/header/Header";
+import SendMessagesForm from "../../components/sendMessagesForm/SendMessagesForm";
+import styles from "./personalchat.module.css";
+import { intAddFriend, intAddPersonalMessage, message } from "../../types";
+import { useEffect, useState } from "react";
+import { useLocation } from "react-router";
+import useRealTimeDB from "../../hooks/useRealTimeDB";
+import { v4 as uuidv4 } from "uuid";
+import Messages from "../../components/messages/Messages";
+import Loading from "react-loading";
 
 const PersonalChat = () => {
   const location = useLocation();
-  const { ownerUid, ownerName, userUid, photo, userName } = location.state || {};
-  
-  const { readUserData, updateUserData } = useRealTimeDB();
-  const [ messages, setMessages ] = useState<message[]>();
-  
-  useEffect(()=>{
-    getData();
-  },[]);
+  const { ownerUid, ownerName, userUid, photo, userName } =
+    location.state || {};
 
-  // get messages from firebase server 
+  const { readUserData, updateUserData } = useRealTimeDB();
+  const [messages, setMessages] = useState<message[]>();
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  // get messages from firebase server
   async function getData() {
     try {
-      const userData = await readUserData<message[]>(`chats/${ownerUid}/${userUid}`);
+      const userData = await readUserData<message[]>(
+        `chats/${ownerUid}/${userUid}`
+      );
       const arr = Object.values(userData as message[]);
       setMessages(arr);
     } catch (error) {
@@ -33,9 +35,14 @@ const PersonalChat = () => {
   }
 
   // write messages to the firebase server
-  const writeChatMessages = async (uidFrom:string,uidTo:string, userName:string, message:string)=>{
+  const writeChatMessages = async (
+    uidFrom: string,
+    uidTo: string,
+    userName: string,
+    message: string
+  ) => {
     const writeData: intAddPersonalMessage = {
-      userDB: 'chats/',
+      userDB: "chats/",
       uidFrom,
       uidTo,
       messageId: uuidv4(),
@@ -46,16 +53,16 @@ const PersonalChat = () => {
     await updateUserData(writeData);
   };
 
-  const writeFriends = async (userUid:string, friendUid:string) =>{
+  const writeFriends = async (userUid: string, friendUid: string) => {
     const writeData: intAddFriend = {
-      userDB: 'friends/',
+      userDB: "friends/",
       userUid,
-      friendUid
+      friendUid,
     };
     await updateUserData(writeData);
   };
 
-  const handlerSendMessages = async (message:string)=>{
+  const handlerSendMessages = async (message: string) => {
     await writeChatMessages(ownerUid, userUid, ownerName, message);
     await writeChatMessages(userUid, ownerUid, ownerName, message);
 
@@ -65,16 +72,20 @@ const PersonalChat = () => {
     await getData();
   };
 
-  return( 
+  return (
     <div className={styles.containerPersonalChat}>
-      <Header props={headerPersonalChats}/>
+      <Header props={headerPersonalChats} styles={{ ...style }} />
       <div className={styles.profileImg}>
         <img src={photo} alt={userName} />
         <label>{userName}</label>
       </div>
       <div className={styles.chatContainer}>
         <div className={styles.chat}>
-          { !messages ? <Loading type="cylon" color="green" className={'loader'} /> : <Messages messages={messages} name={ownerName}/>}
+          {!messages ? (
+            <Loading type="cylon" color="green" className={"loader"} />
+          ) : (
+            <Messages messages={messages} name={ownerName} />
+          )}
         </div>
         <SendMessagesForm
           userName={userName}
@@ -85,3 +96,8 @@ const PersonalChat = () => {
   );
 };
 export default PersonalChat;
+
+const style = {
+  maxHeight: "fit-content",
+  height: "4rem",
+};
