7df73c2b6b3fe590be4e1d6359c55d25dac6cce8
angeldarco <angel92-eme@hotmail.com>
2024-04-24 10:59:53 -0500

    :lipstick: Home section styles updated

--- src/components/profileCard/ProfileCard.tsx
+++ src/components/profileCard/ProfileCard.tsx
@@ -1,46 +1,58 @@
-import styles from './ProfileCard.module.css';
-import Loading from 'react-loading';
-import { intContext } from '../../types';
-import { useNavigate } from 'react-router';
-type contacts = { arr :intContext[], ownerUid: string, ownerName: string };
+import styles from "./ProfileCard.module.css";
+import globalStyles from "../../css/global.module.css";
+import Loading from "react-loading";
+import { intContext } from "../../types";
+import { useNavigate } from "react-router";
+type contacts = { arr: intContext[]; ownerUid: string; ownerName: string };
 interface profilesCard {
-    style:CSSModuleClasses[string];
-    contacts: contacts;
-    showAbout?: boolean;
-    showState?: boolean;
-  limit?: number;}
-const ProfileCard = (props:profilesCard) => {
-  const { 
-    style,
+  contacts: contacts;
+  showAbout?: boolean;
+  showState?: boolean;
+  limit?: number;
+  section?: string;
+}
+const ProfileCard = (props: profilesCard) => {
+  const {
     contacts,
     showAbout = false,
     showState = false,
-    limit } = props;    
-    
+    limit,
+    section,
+  } = props;
+
   // send user to chat data
   const navigate = useNavigate();
-  const handlerClick = (e:React.MouseEvent<HTMLLIElement, MouseEvent>, userUid:string | null)=>{    
+  const handlerClick = (
+    e: React.MouseEvent<HTMLLIElement, MouseEvent>,
+    userUid: string | null
+  ) => {
     const li = e.currentTarget;
     const userData = {
       ownerName: contacts.ownerName,
       ownerUid: contacts.ownerUid,
       userUid,
-      photo: li.querySelector('img')?.src,
-      userName: li.querySelector('span')?.textContent,
+      photo: li.querySelector("img")?.src,
+      userName: li.querySelector("span")?.textContent,
     };
-    navigate('/chat',{ state: userData });
+    navigate("/chat", { state: userData });
   };
 
   return (
-    <div className={styles.containerProfileCard}>
-      <ul className={ style ? style : styles.list}>
+    <div
+      className={`${styles.profileCardContainer} ${section ? styles.rowContainer : ""}`}
+    >
+      <ul className={`${section ? styles.ulRow : ""}`}>
         {!contacts ? (
-          <Loading type="cylon" color="green" className={'loader'} />
+          <Loading type="cylon" color="green" className={"loader"} />
         ) : (
           contacts.arr?.map((item, i) => {
-            if (limit &&i > limit) return;
+            if (limit && i > limit) return;
             return (
-              <li key={i} onClick={(e)=>handlerClick(e,item.userUid)}>
+              <li
+                key={i}
+                onClick={(e) => handlerClick(e, item.userUid)}
+                className={`${!section ? globalStyles.glass : ""} ${section ? styles.liRow : ""}`}
+              >
                 <img src={item.photo} />
                 <div>
                   <span>{item.userName}</span>
