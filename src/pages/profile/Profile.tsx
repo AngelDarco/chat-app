63bf821ddf3abad95217baa258696adcadee8de1
angeldarco <angel92-eme@hotmail.com>
2024-04-24 11:23:35 -0500

    :lipstick: Profile component styles updated

--- src/pages/profile/Profile.tsx
+++ src/pages/profile/Profile.tsx
@@ -1,46 +1,49 @@
-import Header from '../../components/header/Header';
-import styles from './profile.module.css';
-import { FaUserFriends } from 'react-icons/fa';
-import { headerUser } from '../../components/header/Header';
-import { useEffect, useState } from 'react';
-import MessageNoLogged from '../../components/messageNoLog/MessageNoLogged';
-import { Link, useNavigate } from 'react-router-dom';
-import { intContext } from '../../types';
-import Loading from 'react-loading';
-import userContexUpdate from '../../utils/useContextUpdate';
-import useRealTimeDB from '../../hooks/useRealTimeDB';
+import Header from "../../components/header/Header";
+import styles from "./profile.module.css";
+import { FaUserFriends } from "react-icons/fa";
+import { headerUser } from "../../components/header/Header";
+import { useEffect, useState } from "react";
+import MessageNoLogged from "../../components/messageNoLog/MessageNoLogged";
+import { Link, useNavigate } from "react-router-dom";
+import { intContext } from "../../types";
+import Loading from "react-loading";
+import userContexUpdate from "../../utils/useContextUpdate";
+import useRealTimeDB from "../../hooks/useRealTimeDB";
 
 const Profile = (): JSX.Element => {
   const { userContextData } = userContexUpdate();
-  const [ userData, setUserData ] = useState<intContext>();
+  const [userData, setUserData] = useState<intContext>();
   const navigate = useNavigate();
-  //   type contacts = { arr :intContext[], ownerUid: string, ownerName: string };
-  const [ contacts, setContacts ] = useState<intContext[]>();
+
+  const [contacts, setContacts] = useState<intContext[]>();
   /** read all the users profiles */
   useEffect(() => {
     const { readUserData } = useRealTimeDB();
     (async () => {
-      const res = await readUserData('profiles/');
+      const res = await readUserData("profiles/");
       const arr: intContext[] = Object.values(res as intContext);
       setContacts(arr);
     })();
-  }, [ userData?.userUid ]);
+  }, [userData?.userUid]);
 
   /* get user info */
   useEffect(() => {
     userContextData().then((res) => res && setUserData(res));
-  }, [ userData?.userUid ]);
+  }, [userData?.userUid]);
 
-  const handlerClick = (e:React.MouseEvent<HTMLLIElement, MouseEvent>, userUid:string | null)=>{
+  const handlerClick = (
+    e: React.MouseEvent<HTMLLIElement, MouseEvent>,
+    userUid: string | null
+  ) => {
     const li = e.currentTarget;
     const Data = {
       ownerName: userData?.userName,
       ownerUid: userData?.userUid,
       userUid,
-      photo: li.querySelector('img')?.src,
-      userName: li.querySelector('span')?.textContent,
+      photo: li.querySelector("img")?.src,
+      userName: li.querySelector("span")?.textContent,
     };
-    navigate('/chat',{ state: Data });
+    navigate("/chat", { state: Data });
   };
 
   return (
@@ -52,9 +55,13 @@ const Profile = (): JSX.Element => {
         <MessageNoLogged />
       ) : (
         <div className={styles.homeSection}>
-          <Link to={'/profileconfig'}>edit</Link>
+          <Link to={"/profileconfig"}>edit</Link>
           <div className={styles.picture}>
-            <img src={userData?.photo} alt="user-logo" />
+            <img
+              className={styles.mainImg}
+              src={userData?.photo}
+              alt="user-logo"
+            />
             <p>{`${userData?.userName} ${userData?.lastName}`}</p>
             <span>{userData?.state}</span>
           </div>
@@ -62,17 +69,16 @@ const Profile = (): JSX.Element => {
             {userData.about && <span>About me:</span>}
             <p>{userData?.about}</p>
           </div>
+          <div className={styles.title}>
+            <FaUserFriends />
+            <span>Make some Friends</span>
+          </div>
           <div className={styles.friends}>
-            <div  className={styles.title}>
-              <FaUserFriends />
-              <span>
-              Make some Friends</span>
-            </div>
             <ul>
               {contacts &&
                 contacts.map((item, i) => {
                   return (
-                    <li key={i} onClick={(e)=>handlerClick(e,item.userUid)}>
+                    <li key={i} onClick={(e) => handlerClick(e, item.userUid)}>
                       <img src={item.photo} />
                       <div>
                         <span>{item.userName}</span>
