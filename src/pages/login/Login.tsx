836e7752fc1992e281d0b5543af740477cda9df1
angeldarco <angel92-eme@hotmail.com>
2024-04-26 12:15:05 -0500

    :hammer: Login component refactored

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,21 +1,33 @@
-import styles from './login.module.css';
-import { Link } from 'react-router-dom';
-import Header, { headerLogin } from '../../components/header/Header';
-import { AiOutlineGoogle } from 'react-icons/ai';
-import { MdOutlineAlternateEmail } from 'react-icons/md';
+import styles from "./login.module.css";
+import globalStyles from "../../css/global.module.css";
+import { Link } from "react-router-dom";
+import Header, { headerLogin } from "../../components/header/Header";
+import { AiOutlineGoogle } from "react-icons/ai";
+import { MdOutlineAlternateEmail } from "react-icons/md";
 
-const Login = ()=>{
-	return(
-		<div className={styles.containerLogin}>
-			<Header props={ headerLogin } />
-			<div className={styles.logo}>
-				{/* <img src={ico} alt="main-logo" /> */}
-			</div>
-			<div className={styles.links}>
-				<div><Link to={'/email'}><MdOutlineAlternateEmail/> Login with Email</Link></div>
-				<div><Link to={'/gmail'}><AiOutlineGoogle/> Login with Gmail</Link></div>
-			</div>
-		</div>
-	);
+import loginImg from "../../assets/main.gif";
+
+const Login = () => {
+  return (
+    <div className={styles.containerLogin}>
+      <Header props={headerLogin} />
+      <div className={styles.logo}>
+        <img src={loginImg} alt="main-logo" />
+      </div>
+      <div className={`${styles.links} ${globalStyles.glass}`}>
+        <h1>Login</h1>
+        <div className={styles.email}>
+          <Link to={"/email"}>
+            <MdOutlineAlternateEmail /> Login with Email
+          </Link>
+        </div>
+        <div className={styles.gmail}>
+          <Link to={"/gmail"}>
+            <AiOutlineGoogle /> Login with Gmail
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
 };
 export default Login;
