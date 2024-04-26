c2375551a1a60790ffda6a5ebb538994300f7118
angeldarco <angel92-eme@hotmail.com>
2024-04-26 12:21:39 -0500

    :racehorse: Logout component refactored

--- src/pages/logout/Logout.tsx
+++ src/pages/logout/Logout.tsx
@@ -1,62 +1,58 @@
-import { useNavigate } from 'react-router';
-import Swal from 'sweetalert2';
-import Loading from 'react-loading';
-import userContexUpdate from '../../utils/useContextUpdate';
+import globalStyles from "../../css/global.module.css";
+import { useNavigate } from "react-router";
+import Swal from "sweetalert2";
+import Loading from "react-loading";
+import userContexUpdate from "../../utils/useContextUpdate";
 
 const Logout = () => {
-	const { deleteUserContext } = userContexUpdate();
-	const navigate = useNavigate();
-	
-	const uid = window.localStorage.getItem('chatDarcoUserUid');
-	
-	const handlerLogout = () => {		
-		uid ?
-			Swal.fire({
-				title: 'You wanna close session?',
-				icon: 'warning',
-				showCancelButton: true,
-				confirmButtonColor: '#3085d6',
-				confirmButtonText: 'Yes'
-			}).then((result) => {
-				if (result.isConfirmed) {
-					deleteUserContext()
-						.then(res => {
-							!res ?
-								(Swal.fire({
-									title: 'Session Closed',
-									icon: 'success',
-									showConfirmButton: false,
-									timer: 1000
-								}
-								), setTimeout(() => navigate('/'), 2000))
-								: (Swal.fire({
-									title: res,
-									icon: 'error',
-									timer: 1000
-								}), setTimeout(() => navigate(-1), 2000));
-						});
-				}else{ navigate(-1); }
-			})	: setTimeout(() => navigate('/'), 1000);
-		return <Loading
-			type='cylon'
-			color='green'
-			className={styles.style.toString()}
-		/>;
-	};
+  const { deleteUserContext } = userContexUpdate();
+  const navigate = useNavigate();
 
-	return(
-		<div style={styles.style}>
-			{ handlerLogout() }
-		</div>
-	);
+  const uid = window.localStorage.getItem("chatDarcoUserUid");
+
+  const handlerLogout = () => {
+    uid
+      ? Swal.fire({
+          title: "You wanna close session?",
+          icon: "warning",
+          showCancelButton: true,
+          confirmButtonColor: "#3085d6",
+          confirmButtonText: "Yes",
+        }).then((result) => {
+          if (result.isConfirmed) {
+            deleteUserContext().then((res) => {
+              !res
+                ? (Swal.fire({
+                    title: "Session Closed",
+                    icon: "success",
+                    showConfirmButton: false,
+                    timer: 1000,
+                  }),
+                  setTimeout(() => navigate("/"), 2000))
+                : (Swal.fire({
+                    title: res,
+                    icon: "error",
+                    timer: 1000,
+                  }),
+                  setTimeout(() => navigate(-1), 2000));
+            });
+          } else {
+            navigate(-1);
+          }
+        })
+      : setTimeout(() => navigate("/"), 1000);
+    return (
+      <div className={globalStyles.loader}>
+        <Loading type="cylon" color="green" />
+      </div>
+    );
+  };
+
+  return <div style={{ ...styles }}>{handlerLogout()}</div>;
 };
 export default Logout;
-const styles={
-	style: {
-		width: '100%',
-		height: '100vh',
-		display: 'flex',
-		justifyContent: 'center',
-		padding: '50px 0',
-	}
+const styles = {
+  width: "var(--width)",
+  minWidth: "var(--min-width)",
+  height: "var(--height)",
 };
