91f8c187f15a609fdd236158c69eb9332ef4d0eb
angeldarco <angel92-eme@hotmail.com>
2024-04-27 00:09:16 -0500

    :racehorse: Welcome section component refactored

--- src/pages/welcome/Welcome.tsx
+++ src/pages/welcome/Welcome.tsx
@@ -1,27 +1,57 @@
-import styles from './welcome.module.css';
-import ico from '../../assets/img/ico.png';
-import { Link } from 'react-router-dom';
-import { VscSignIn } from 'react-icons/vsc';
-import { FaUserPlus } from 'react-icons/fa';
-import { HiChatBubbleLeftRight } from 'react-icons/hi2';
+/* eslint-disable react/no-unknown-property */
+import styles from "./welcome.module.css";
+import { Link } from "react-router-dom";
+import { VscSignIn } from "react-icons/vsc";
+import { FaUserPlus } from "react-icons/fa";
+import { HiChatBubbleLeftRight } from "react-icons/hi2";
 
-const Welcome = ():JSX.Element =>{
-	return(
-		<div className={styles.containerMain}>
-			<div className={styles.mainLogo}>
-				<img src={ico} alt="main-logo" />
-			</div>
-			<div className={styles.mainWelcomeText}>
-				<h1>The Best online chat that you could find today</h1>
-				<span> You can choice login if you already have an account, register and create and account or just try the chat with the opcion: <p> <Link to={'public'}><HiChatBubbleLeftRight />Just check it out</Link></p> </span>
-			</div>
-			<div className={styles.mainLoginOptions}>
-				<Link to={'/login'} > <VscSignIn/> Login</Link>
-				<Link to={'/register'} > <FaUserPlus />New User? Sign Up</Link>
-				<Link to={'/public'} > <HiChatBubbleLeftRight />Just check it out</Link>
-			</div>
-		</div>
-	);
+import img from "../../assets/main.gif";
+import mainLogo from "../../assets/main.mp4";
+import { useEffect, useRef } from "react";
+
+const Welcome = (): JSX.Element => {
+  const videoRef = useRef<HTMLVideoElement>(null);
+
+  useEffect(() => {
+    if (videoRef.current) videoRef.current.playbackRate = 0.3;
+  }, []);
+
+  return (
+    <div className={styles.containerMain}>
+      <img src={img} alt=" main-logo" width={300} />
+
+      <video autoPlay loop muted ref={videoRef}>
+        <source src={mainLogo} type="video/mp4" />
+      </video>
+
+      <div className={styles.containerText}>
+        <div className={styles.textMain}>
+          <h1> Welcome to our chat app</h1>
+          <p>
+            Login or register to start using our app, and connect with your
+            friends around the world
+          </p>
+        </div>
+
+        <div className={styles.linksMain}>
+          <Link to={"/login"}>
+            {" "}
+            <VscSignIn /> Login
+          </Link>
+          <Link to={"/register"}>
+            {" "}
+            <FaUserPlus />
+            New User? Sign Up
+          </Link>
+          <Link to={"/public"}>
+            {" "}
+            <HiChatBubbleLeftRight />
+            Check it out
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Welcome;
