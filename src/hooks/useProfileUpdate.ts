8c1d853d50e1ef408398df502958d68057900eed
Angel Darco <angel92-eme@hotmail.com>
2025-01-21 23:55:53 -0500

    :hammer: custom hook to update users profiles updated

--- src/hooks/useProfileUpdate.ts
+++ src/hooks/useProfileUpdate.ts
@@ -1,26 +1,24 @@
-import { getDownloadURL, getStorage, ref, uploadBytes } from 'firebase/storage';
-import useRealTimeDB from './useRealTimeDB';
+import { getDownloadURL, getStorage, ref, uploadBytes } from "firebase/storage";
+import useRealTimeDB from "./useRealTimeDB";
 
 const useProfileUpdate = () => {
-	const { writeUserData, readUserData } = useRealTimeDB();
+  const { writeUserData, readUserData } = useRealTimeDB();
 
-	const storageImgs = async (fileName: string, file: Blob | undefined):Promise<string | undefined> =>{
-		if(!file) return;
-		try {
-			const storage = getStorage();
-			const imagesRef = ref(storage, 'profiles/' + fileName);
-			await uploadBytes(imagesRef, file);
-			const res = await getDownloadURL(ref(imagesRef));
-			return res;
-		} catch(error) {
-			return (error as string);
-		}
-	};
-
-
-
-
-	return { storageImgs, writeUserData, readUserData };
+  const storageImgs = async (
+    fileName: string,
+    file: Blob | undefined
+  ): Promise<string | undefined> => {
+    if (!file) return;
+    try {
+      const storage = getStorage();
+      const imagesRef = ref(storage, "profiles/" + fileName);
+      await uploadBytes(imagesRef, file);
+      const res = await getDownloadURL(ref(imagesRef));
+      return res;
+    } catch (error) {
+      return error as string;
+    }
+  };
+  return { storageImgs, writeUserData, readUserData };
 };
 export default useProfileUpdate;
-
