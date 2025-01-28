f27b2c28d3d7b4ab9b5ce3efd5dd69885d53109a
Angel Darco <angel92-eme@hotmail.com>
2025-01-28 02:44:23 -0500

    :hammer: useLoginUsers hook, code refactored

--- src/hooks/useLoginUsers.ts
+++ src/hooks/useLoginUsers.ts
@@ -1,32 +1,51 @@
-import { getAuth, signInWithEmailAndPassword, signInWithPopup, GoogleAuthProvider, User, signOut } from 'firebase/auth';
-import { intLoginUserData } from '../types';
+import {
+  getAuth,
+  signInWithEmailAndPassword,
+  signInWithPopup,
+  GoogleAuthProvider,
+  User,
+  signOut,
+} from "firebase/auth";
+import { app } from "../firebase/firebase-config";
+import { LoginData } from "../types";
 
-const auth = getAuth();
+const auth = getAuth(app);
 
 const useLoginUsers = () => {
-	interface intData extends User { message?: string }
-	const loginWithEmail = async ({ email, password }: intLoginUserData): Promise<intData> => {
-		try {
-			const userCredentials = await signInWithEmailAndPassword(auth, email, password);
-			return userCredentials.user;
-		} catch (err) {
-			return err as intData;
-		}
-	};
+  interface intData extends User {
+    message?: string;
+  }
+  const loginWithEmail = async ({
+    email,
+    password,
+  }: LoginData): Promise<intData> => {
+    try {
+      const userCredentials = await signInWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
+      return userCredentials.user;
+    } catch (err) {
+      return err as intData;
+    }
+  };
 
-	const loginWithGmail = () => {
-		const provider = new GoogleAuthProvider();
-		return signInWithPopup(auth, provider);
-	};
+  const loginWithGmail = () => {
+    const provider = new GoogleAuthProvider();
+    return signInWithPopup(auth, provider);
+  };
 
-	const logout = async () => {
-		await signOut(auth).then((res) => {
-			return res;
-		}).catch((error) => {
-			throw new Error(error);
-		});
-	};
+  const logout = async () => {
+    await signOut(auth)
+      .then((res) => {
+        return res;
+      })
+      .catch((error) => {
+        throw new Error(error);
+      });
+  };
 
-	return { loginWithEmail, loginWithGmail, logout };
+  return { loginWithEmail, loginWithGmail, logout };
 };
 export default useLoginUsers;
